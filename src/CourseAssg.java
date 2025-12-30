cea585f0b2747f20e90ec4272297388b4c630f45
dayangnurnazihah <dayangziha@gmail.com>
2025-12-30 10:51:02 +0800

    project init

---
+++ src/CourseAssg.java
@@ -0,0 +1,17 @@
+package src;
+class CourseAssg {
+  private Course course;
+  private String session;
+  private int semester;
+  
+  public CourseAssg(Course course, String session, int semester) {
+    this.course = course;
+    this.session = session;
+    this.semester = semester;
+  }
+  
+  public Course getCourse() {
+    return course;
+  }
+
+}
